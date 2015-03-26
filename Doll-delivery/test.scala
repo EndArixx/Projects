d0dcc012335f2f0b2f49ec9fec8942090d003306
EndArixx <HendArix@gmail.com>
2015-03-26 12:15:18 -0400

    more tests

--- Doll-delivery/test.scala
+++ Doll-delivery/test.scala
@@ -1,6 +1,14 @@
+import scala.io.Source
+
 object test {
   def main(args: Array[String]) {
     println("Wake up Neo!")
+    
+    val filename = "randomdata.txt"
+    for(line <- Source.fromFile(filename).getLines())
+    {
+        println(line)
+    }
   }
 }
 
