394584cf374954db58fa9cce9bb031f5a5eb88b5
endarixx <HendArix@gmail.com>
2015-05-01 22:42:10 -0400

    Prep
    more prep work

--- Bosch Assignment/src/run.java
+++ Bosch Assignment/src/run.java
@@ -1,29 +1,34 @@
 
 public class run 
 {
+	
+	/*
+	 * 	This is a Program written by John Stanley
+	 */
 	void main() 
 	{
-		/*- create new thread for server (listen_thread)
-		      - start server to listen on tcp port 9090
-		- on every incoming request, move request to its
+		/*- create new thread for server (listen_thread) 			[]
+		- start server to listen on tcp port 9090					[]
+		- on every incoming request, move request to its			[]
 		 own new thread, say request_handler thread,
 		  so listen_thread is back to listening
 		  
-		- For incoming requests,
+		- For incoming requests,									[]
 		 the request_handler thread waits for a string
 		  to be sent terminated by newline, 
 		  for eg. "Hello World!\n" and the server then 
 		  responds with string "ACK:" plus the original message,
 		  for eg. "ACK:Hello World!"
-		     	
-		- while (true) {
-		- create client and establish a tcp connection to server listening on port 9090 locally
-		- Send a timestamp string of form "yyyy-MM-dd HH:mm:ss" followed by newline character
-		- Print to console what was sent and response from server, on separate lines.
-		- send a message to server of form "Request-XX" where XX should be a random number between 00 and 99
-		- print what was sent and response from server, on separate lines.
-		            - close connection to server
-		 }*/
+		  */
+		
+		//- while (true) {
+		//- create client and establish a tcp connection to server listening on port 9090 locally
+		//- Send a timestamp string of form "yyyy-MM-dd HH:mm:ss" followed by newline character
+		//- Print to console what was sent and response from server, on separate lines.
+		//- send a message to server of form "Request-XX" where XX should be a random number between 00 and 99
+		//- print what was sent and response from server, on separate lines.
+		//- close connection to server
+		//�}
 		
 		
 		
