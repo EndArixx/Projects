948f742ddbb312f827de59d9c6a08148a61937ab
endarixx <HendArix@gmail.com>
2015-05-03 13:48:02 -0400

    Commenting and socket memory fix
    if the program runs to quickly then it uses up the sockets before they
    can be properly freed

--- Bosch Assignment/src/JStanleyPort.java
+++ Bosch Assignment/src/JStanleyPort.java
@@ -28,17 +28,19 @@ main() {
 
 public class JStanleyPort
 {
-	public static boolean debug = false;
+		//This is for debugging.
+		//when set to true the program will print out useful information to the console. 
+		//things like threads starting and closing
+	public static boolean debug = true;
 	
 	class request_handler implements Runnable
 	{
 		private String name;
 		private Thread thread;
-		private PrintWriter pw;
-		private BufferedReader br;
 		Socket socket;
 		request_handler(String name, Socket socket )
-		{
+		{		
+				//although the name isn't required, it is very useful for testing and seeing which threads are doing what.
 			this.name = name;
 			this.socket = socket;
 		}
@@ -53,18 +55,21 @@ public class JStanleyPort
 		  	for eg. "ACK:Hello World!"
 			 */
 			try
-			{
+			{	
+				
+					//set up I/O Steams 
+					//	Input
 				InputStreamReader inputstreamreader = new InputStreamReader(socket.getInputStream());
 			    BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
-			      
+			      	//	Output
 			    PrintWriter printwriter = new PrintWriter(socket.getOutputStream(),true);
-			 
-			      
-			      
+			    
 			    String line = "";
-			      
+			     
+			    	//as long as the input isn't null and still open.
 			    while ((line = bufferedreader.readLine()) != null)
 			    {
+			    		//take what was provided and return it with "ACK:" on the front.
 			    	printwriter.println("ACK:" + line);
 			    }
 			      
@@ -75,18 +80,17 @@ public class JStanleyPort
 			    printwriter.close();
 			    socket.close();
 			}
-			catch(UnknownHostException unhe){
-		      System.out.println("UnknownHostException: " + unhe.getMessage());
-		    }catch(InterruptedIOException intioe){
-		      System.out.println("Timeout while attempting to establish socket connection.");
-		    }catch(IOException ioe){
-		      System.out.println("IOException: " + ioe.getMessage());
-		    }finally{
+				//Error Handlers 
+			catch(UnknownHostException unhe){System.out.println("request_handler - UnknownHostException: " + unhe.getMessage());}
+			catch(InterruptedIOException intioe){System.out.println("request_handler - Timeout while attempting to establish socket connection.");}
+			catch(IOException ioe){System.out.println("request_handler - IOException: " + ioe.getMessage());}
+			finally
+			{
 		      try{
+		    	  	//ensure it closes even if the program had an error
 		        socket.close();
-		      }catch(IOException ioe){
-		    	  if(debug){System.out.println("IOException: " + ioe.getMessage());}
 		      }
+		      catch(IOException ioe){System.out.println("request_handler - IOException: " + ioe.getMessage());}
 		    }
 			
 		}
@@ -132,25 +136,28 @@ public class JStanleyPort
 					      	//wait for client
 					    if((socket = serversocket.accept()) != null)
 					    {
-				    		socket.setSoTimeout(10000);
+
+				    		socket.setSoTimeout(1000);
+			    				//this creates a new thread and gives that thread the socket
 					    	new request_handler("r" + n, socket).start();
+					    		//for testing reasons each handler thread is given a unique number so they can be identified.
 					    	n++;
 					    }
 			    	}
-			    }catch(UnknownHostException unhe){
-			      System.out.println("UnknownHostException: " + unhe.getMessage());
-			    }catch(InterruptedIOException intioe){
-			      System.out.println("Timeout while attempting to establish socket connection.");
-			    }catch(IOException ioe){
-			      System.out.println("IOException: " + ioe.getMessage());
-			    }finally{
-			      try{
+			    }
+			    	//Error Handlers
+			    catch(UnknownHostException unhe){System.out.println("listen_thread - UnknownHostException: " + unhe.getMessage());}
+			    catch(InterruptedIOException intioe){System.out.println("listen_thread - Timeout while attempting to establish socket connection.");}
+			    catch(IOException ioe){System.out.println("listen_thread - IOException: " + ioe.getMessage());}
+			    finally{
+			      try
+			      {
+			    	  	//close everything thats still open.
 			    	  if(debug){System.out.println("    Closing listen_thread: " +  name );}
-			        socket.close();
-			        serversocket.close();
-			      }catch(IOException ioe){
-			        System.out.println("IOException: " + ioe.getMessage());
+			    	  socket.close();
+			    	  serversocket.close();
 			      }
+			      catch(IOException ioe){System.out.println("listen_thread - IOException: " + ioe.getMessage());}
 			    }
 		}
 		public void start ()
@@ -158,6 +165,7 @@ public class JStanleyPort
 			if(debug){System.out.println("    Starting listen_thread: " +  name );}
 	      if (thread == null)
 	      {
+				//start the thread
 	         thread = new Thread (this, name);
 	         thread.start ();
 	      }
@@ -171,19 +179,33 @@ public class JStanleyPort
 		if(debug){System.out.println("  Starting Program");}
 		JStanleyPort j1 = new JStanleyPort();
 		
-		listen_thread l1 = j1.new listen_thread("Thread one");
-		l1.start();
 		
+			//this will be the Listen Thread.
+		listen_thread l1 = j1.new listen_thread("Listen Thread one");
+		l1.start();
+			
+			//variables that will be needed
+	    String line = "";
+	    int number;
+	    Random rn = new Random();
 		Socket socket = null;
+
+			//desired port
 		int serverport = 9090;
 		try 
 		{
-		    for(int i = 0; i < 10000; i++)
+				//while(true)
+				//	this is dangerous but the specifications asked for it.
+		    for(int i = 0; i < 1000000; i++)
 		    {
-				//- create client and establish a tcp connection to server listening on port 9090 locally
+		    	long start = System.nanoTime();
+		    	long wait = 10000000;
+		    	
+		    	
+					//- create client and establish a tcp connection to server listening on port 9090 locally
 		
-				//try to open a socket on port 9090
-				//InetAddress.getByName(null) is local
+					//try to open a socket on port 9090
+					//InetAddress.getByName(null) is local
 		    	if(debug){System.out.println("  Connecting to " + InetAddress.getByName(null) + " on port " + serverport);}
 			    socket = new Socket(InetAddress.getByName(null),serverport);
 			    
@@ -192,17 +214,17 @@ public class JStanleyPort
 			    if(debug){System.out.println("  Client Connected.");}
 			    
 			    	//Input Reader
+			    	//	This will handle all the incoming data through the port.
 			    InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
 			    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 			    	//Output Writer 
+			    	//	This will send data up through the port.
 			    PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
 			    
 			    
 	
 			    	
-			    String line = "";
-			    int number;
-			    
+
 			    	//Start listening to the port
 			    
 					//- Send a timestamp string of form "yyyy-MM-dd HH:mm:ss" followed by newline character
@@ -210,49 +232,57 @@ public class JStanleyPort
 			    DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			    Date date = new Date();
 			    String timestamp = format.format(date);
-			    
+			    	//send the date after formatting
 			    printWriter.println(timestamp);
 			    System.out.println(timestamp);
+			    	//print what was returned by the server.
 			    line = bufferedReader.readLine();
 			    System.out.println(line);
 			    
 	
 					//- send a message to server of form "Request-XX" where XX should be a random number between 00 and 99
 					//- print what was sent and response from server, on separate lines.
-	
-			    Random rn = new Random();
+			    
+			    	//random number between 0 and 99
 			    number = rn.nextInt(100);
+			    	//sends the data up the stream
 				printWriter.println("Request-" + number);
-				System.out.println("Request-" + number); 
+				System.out.println("Request-" + number);
+					//prints what was returned by the server
 				line = bufferedReader.readLine();
 			    System.out.println(line);
-			    
-			    
-			    //- close connection to server
+			    			    
+			    	//- close connection to server
+			    	//	close all readers,writers and the socket.
 			    if(debug){System.out.println("  Client Closing connection.");}
 			    bufferedReader.close();
 			    inputStreamReader.close();
 			    printWriter.close();
 			    socket.close();
+			    while(System.nanoTime() < start + wait)
+			    {
+			    	//when testing on a Windows 7 AMD FX(tm)-4100 Quad-core processor 
+			    	//the program ran to quickly and the operating system could not free sockets 
+			    	//fast enough for the program. so it had to be slowed down.
+			    	//this was done by ensuring the program would always take at least 10 milliseconds.
+			    }
 		    }
+		    
 		}
-		catch(UnknownHostException unhe){
-	      System.out.println("UnknownHostException: " + unhe.getMessage());
-	    }catch(InterruptedIOException intioe){
-	      System.out.println("Timeout while attempting to establish socket connection.");
-	    }catch(IOException ioe){
-	      System.out.println("IOException: " + ioe.getMessage());
-		}finally{
-	      try{
+			//Error Handlers
+		catch(UnknownHostException unhe){System.out.println("Client - UnknownHostException: " + unhe.getMessage());}
+		catch(InterruptedIOException intioe){System.out.println("Client - Timeout while attempting to establish socket connection.");}
+		catch(IOException ioe){System.out.println("Client - IOException: " + ioe.getMessage());}
+		finally
+		{
+	      try
+	      {
 	        socket.close();
-	      }catch(IOException ioe){
-	        System.out.println("IOException: " + ioe.getMessage());
 	      }
-			
-			
-			
+	      catch(IOException ioe){System.out.println("Client - IOException: " + ioe.getMessage());}	
 		}
 		if(debug){System.out.println("Closing Program");}
+			//ensure all threads close properly.
 		System.exit(0);
 	}
 }
