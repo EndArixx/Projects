d88d81d77361e791484c4a34506c7d435d111d4b
John Mark Stanley <endarixx@users.noreply.github.com>
2018-08-25 11:05:03 -0400

    Updated html base
    Removed a bunch of redundancy in the HTML base
    
    Updated teh nav bars so i can pass them around and use them easily

--- Table Top RPG Stat Website/Core/Core/urls.py
+++ Table Top RPG Stat Website/Core/Core/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-	path('stats/', include('stats.urls')),
+	path('RPG/', include('stats.urls')),
 	path('admin/', admin.site.urls),
 ]
