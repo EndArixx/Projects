59e56eaf2c6073154b2ebb7956ce81650f7ed5e5
John Mark Stanley <endarixx@users.noreply.github.com>
2018-10-13 17:12:49 -0400

    Moving to new Repo.
    The website has moved to the following repo:
    
    https://github.com/EndArixx/CoreSite

--- Table Top RPG Stat Website/Core/stats/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class CharacterForm(forms.Form):
-	MIND_stat_f = forms.IntegerField(label='Mind Stat',required=False)
-	FIST_stat_f = forms.IntegerField(label='Fist Stat',required=False)
-	EYES_stat_f = forms.IntegerField(label='Eyes Stat',required=False)
-	FACE_stat_f = forms.IntegerField(label='Face Stat',required=False)
-	HEART_stat_f = forms.IntegerField(label='Heart Stat',required=False)
